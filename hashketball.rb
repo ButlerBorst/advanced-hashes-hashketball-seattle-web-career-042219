cf6be076ec10a830941d53306b70dcd00d520eb6
Alex Borst <butler.borst@gmail.com>
2019-04-19 00:24:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,19 @@
 # Write your code here!
 
-
+def game_hash
+  hash = {
+    :home => {
+      :team_name => []
+      :colors => []
+      :players => {"name" => "stats"}
+    }
+    
+    :away => {
+      :team_name => []
+      :colors => []
+      :players => {"name" => "stats"}
+    }
+  }
 
 
 
